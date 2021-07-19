d9bdc104ca6ce42914803e9e455822765b63ffe6
Shirley-Hxn <2689767443@qq.com>
2021-07-19 17:55:58 +0800

    111

--- a_01_九九乘法表.py
+++ a_01_九九乘法表.py
@@ -1,5 +1,5 @@
 def multiple_table():
-
+    """封装函数"""
     # 外循环控制行，内循环控制列
     row = 1
     while row <=9:
@@ -11,5 +11,5 @@ def multiple_table():
             col += 1
         print("")  # 勿遗漏：打印行
         row += 1
-
-multiple_table()  #调用封装好的函数
+# 调用封装好的函数
+multiple_table()
