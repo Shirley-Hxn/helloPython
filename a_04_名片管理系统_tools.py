c06060bb1f715c23169c8bd5b6261ce298f8354a
Shirley-Hxn <2689767443@qq.com>
2021-07-24 22:50:24 +0800

    Create a_04_名片管理系统_tools.py

---
+++ a_04_名片管理系统_tools.py
@@ -0,0 +1,29 @@
+def show_menu():
+    """显示功能菜单"""
+    print('*' * 50)
+    print('欢迎使用【名片管理系统】V 1.0')
+    print("")
+    print("1、新建名片")
+    print("2、显示全部")
+    print("3、查询名片")
+    print("")
+    print("0、退出系统")
+    print('*' * 50)
+
+
+def new_card():
+    """新增名片"""
+    print("-" * 50)
+    print("新增名片")
+
+
+def show_all():
+    """显示全部"""
+    print("-" * 50)
+    print("显示全部")
+
+
+def search_card():
+    """搜索名片"""
+    print("-" * 50)
+    print("搜索名片")
