7edd0121cbf371ec9985405220aa8c376df2acdc
Shirley-Hxn <2689767443@qq.com>
2021-07-24 22:50:38 +0800

    Update a_04_名片管理系统_main.py

--- a_04_名片管理系统_main.py
+++ a_04_名片管理系统_main.py
@@ -1,26 +1,29 @@
+import a_04_名片管理系统_tools
+
 
 while True:
 
-    # 显示功能菜单
+    # TODO 显示功能菜单
+    a_04_名片管理系统_tools.show_menu()
 
     action_str = input("请选择要执行的操作： ")
     print("您选择的操作是【%s】" % action_str)
 
-    if action_str in ['1','2','3']:
+    if action_str in ['1', '2', '3']:
+        """ 功能：
+        1、新建名片
+        2、显示全部
+        3、查询名片
+        0、退出系统
         """
-            功能：
-            1、新建名片
-            2、显示全部
-            3、查询名片
-
-            0、退出系统
-            """
         if action_str == '1':
-            pass
+            a_04_名片管理系统_tools.new_card()
+
         elif action_str == '2':
-            pass
+            a_04_名片管理系统_tools.show_all()
+
         elif action_str == '3':
-            pass
+            a_04_名片管理系统_tools.new_card()
 
         pass  # 占位符；不执行任何操作
 
